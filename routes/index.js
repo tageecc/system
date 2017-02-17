bb3405335486c5461b444168426fddc2ac10c48d
tagee <zhangzexuan@4399inc.com>
2017-02-17 17:43:05 +0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let router = express.Router();
+let _ = require('lodash');
+let ps = require('current-processes');
+
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+router.get('/info', function (req, res, next) {
+    ps.get(function (err, processes) {
+
+        let sorted = _.sortBy(processes, 'cpu');
+        let top = sorted.reverse().splice(0, 5);
+
+        res.json(top);
+    });
+
+});
+
+
+module.exports = router;
