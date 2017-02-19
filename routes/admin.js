1bc6bacea0d620077d816e1d04f2b05850756178
tagee <896932646@qq.com>
2017-02-19 23:03:56 +0800

    1

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,29 @@
 let express = require('express');
 let router = express.Router();
 let _ = require('lodash');
+let os = require("os");
+var osUtils = require("os-utils");
 let ps = require('current-processes');
 
 router.get('/', function (req, res, next) {
     res.redirect('/admin/process');
 });
 router.get('/process', function (req, res, next) {
-    res.render('admin/admin-process');
+    res.render('admin/admin-process',{cur:'process'});
 });
-
+router.get('/performance', function (req, res, next) {
+    res.render('admin/admin-performance',{cur:'performance'});
+});
 router.post('/process', function (req, res, next) {
     ps.get(function (err, processes) {
         let top = _.orderBy(processes, req.body.sort_type, req.body.sort_order == 1 ? 'asc' : 'desc');
         res.json(top);
     });
-
 });
-
+router.post('/performance', function (req, res, next) {
+    osUtils.cpuUsage(function (value) {
+        res.json(value);
+    });
+});
 
 module.exports = router;
