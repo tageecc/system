417ee0f37825834b6e7611c00b91bf9a173809a9
tagee <zhangzexuan@4399inc.com>
2017-02-23 18:52:07 +0800

    1

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+let fs = require("fs");
 let _ = require('lodash');
 let os = require("os");
 let osUtils = require("os-utils");
@@ -45,17 +46,40 @@ router.get('/sysinfo', function (req, res, next) {
 });
 
 router.get('/nginx', function (req, res, next) {
-    let exist = exec('rpm -q nginx').stdout.length != 0;
-
-    res.render('admin/admin-nginx', {exist: exist, cur: 'nginx'});
+    let data = {};
+    let nginx = exec('nginx -V', {silent: true});
+    data.exist = nginx.code == '0';
+    if (data.exist) {
+        data.version = nginx.stderr.match(/nginx\/[0-9]+\.[0-9]+\.[0-9]+/);
+        data.conf = nginx.stderr.match(/--conf-path=(\S+)\s/)[1]
+    }
+    res.render('admin/admin-nginx', {data: data, cur: 'nginx'});
 });
 /**
  * 安装nginx
  */
 router.get('/nginx/install', function (req, res, next) {
 
-
     res.render('admin/admin-nginx', {cur: 'nginx'});
+});
+
+
+router.get('/website', function (req, res, next) {
+    let data = {};
+    let nginx = exec('nginx -V', {silent: true});
+    data.exist = nginx.code == '0';
+    if (data.exist) {
+        data.conf = nginx.stderr.match(/--conf-path=(\S+)\s/)[1];
+
+        let config = fs.existsSync(data.conf) ? fs.readFileSync(data.conf, "utf-8") : '';
+        config.split('\n').filter(item =>{
+            console.log(item[0]);
+            return  item[0] != '#';
+        }).join('');
+        console.log(config);
+    }
+    console.log(data);
+    res.render('admin/admin-website', {data: data, cur: 'website'});
 });
 
 module.exports = router;
