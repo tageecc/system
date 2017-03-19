bdc3b41d184d99eb09e72bf899bed52138c4686a
tagee <896932646@qq.com>
2017-03-19 16:57:15 +0800

    修改模板引擎为handlebars

--- app.js
+++ app.js
@@ -4,26 +4,27 @@ let favicon = require('serve-favicon');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
+let exphbs  = require('express-handlebars');
 let sse = require('./middleware/sse');
 require('shelljs/global');
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.set('view cache', false);
-app.engine('ejs', require('ejs-mate'));
+app.engine('hbs', exphbs({
+    layoutsDir: 'views',
+    partialsDir:"views",
+    defaultLayout: 'layout',
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// 定义视图全局变量
 
 // 定制中间件
 app.use(sse);
